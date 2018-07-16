c0d3bd2ff122e9be1ac0b739de065a412a2e7712
rodrigo-cl <40841435+rodrigo-cl@users.noreply.github.com>
2018-07-16 15:08:21 -0300

    Update aula 1 ex1.cpp

--- aula 1 ex1.cpp
+++ aula 1 ex1.cpp
@@ -4,11 +4,12 @@ int main()
 
 {
   const char *p = "\"setor\":\"alimentos\"";
-
+  char forb[] = {0x22, 0x3a};
+  
   while (*p != 0x00)
   {
     
-    if (*p != 0x22)
+    if (*p != forb[0] && *p != forb[1])
     printf("%c", *p);
     if (*p == 0x3a)
     printf("\n");
